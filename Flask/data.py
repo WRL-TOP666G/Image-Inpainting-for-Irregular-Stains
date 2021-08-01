fa3f2723d67d6e6a5cdcbf92813ec76393a2efd8
WeiRu-Bob <a900240@gmail.com>
2021-07-31 17:36:32 -0700

    Update Flask

---
+++ Flask/data.py
@@ -0,0 +1,36 @@
+def Articles():
+    articles = [
+        {
+            'id': 1, 
+            'title': 'Article One',
+            'body': 'Hi there!',
+            'author': 'Wei', 
+            'create_date': '06-30-2021'
+        },
+
+        {
+            'id': 2, 
+            'title': 'Article Two',
+            'body': 'Hi there 2!',
+            'author': 'Wei', 
+            'create_date': '06-30-2021'
+        },
+        
+        {
+            'id': 3, 
+            'title': 'Article Three',
+            'body': 'Hi there 3!',
+            'author': 'Wei', 
+            'create_date': '06-30-2021'
+        },
+        
+        {
+            'id': 4, 
+            'title': 'Article Four',
+            'body': 'Hi there 4!',
+            'author': 'Wei', 
+            'create_date': '06-30-2021'
+        },
+    ]
+
+    return articles
